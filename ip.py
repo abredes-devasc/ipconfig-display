92852782b6d93be9976e2420803352b273c348df
Julio Damasceno <xjulio@gmail.com>
2020-10-26 05:41:38 +0000

    Removendo palavra Codigo na linha 5

--- ip.py
+++ ip.py
@@ -2,7 +2,7 @@ import requests
 
 site = "https://ifconfig.co/ip"
 response = requests.get(url=site)
-print(f"Codigo HTTP: {response.status_code}")
+print(f"HTTP: {response.status_code}")
 print(f"IP: {response.text}")
 
 site = "https://ifconfig.co/asn"
