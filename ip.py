61e43324bdb87a00854c5c5df4a5ee20a46ea534
Julio Damasceno <xjulio@gmail.com>
2020-10-26 04:36:41 +0000

    Imprimindo ASN

--- ip.py
+++ ip.py
@@ -5,3 +5,7 @@ response = requests.get(url=site)
 
 print(f"Codigo HTTP: {response.status_code}")
 print(f"Meu endereço IP: {response.text}")
+
+site = "https://ifconfig.co/asn"
+response = requests.get(url=site)
+print(f"Meu ASN: {response.text}")
