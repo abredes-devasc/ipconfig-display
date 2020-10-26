389ec6b6b8f7c16eaad66c807f503185efe810a1
Julio Damasceno <xjulio@gmail.com>
2020-10-26 04:44:15 +0000

    Incluindo codigo http para chamado do ASN

--- ip.py
+++ ip.py
@@ -8,4 +8,5 @@ print(f"Meu endereço IP: {response.text}")
 
 site = "https://ifconfig.co/asn"
 response = requests.get(url=site)
+print(f"Codigo HTTP: {response.status_code}")
 print(f"Meu ASN: {response.text}")
